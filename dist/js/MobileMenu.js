8cdf1e7ce0e04806f870f0ec63f625f0a702dfa5
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2021-11-06 20:13:07 -0500

    modified header styles

--- dist/js/MobileMenu.js
+++ dist/js/MobileMenu.js
@@ -1,7 +1,6 @@
 const sidebar = document.querySelector(".sidebar");
 const mobileOverlay = document.querySelector(".mobile-overlay");
 const sidebarIcon = document.querySelector(".sidebar__icon");
-const container = document.querySelector(".container");
 const mobileLinks = document.querySelectorAll(".sidebar__link");
 
 export const handleIconClick = () => {
@@ -9,7 +8,6 @@ export const handleIconClick = () => {
   mobileOverlay.style.display = "block";
   sidebarIcon.addEventListener("click", closeSidebar);
   mobileOverlay.addEventListener("click", closeSidebar);
-  container.classList.add("no-scrolling");
 
   for (let i = 0; i < mobileLinks.length; i++) {
     let link = mobileLinks[i];
@@ -19,6 +17,5 @@ export const handleIconClick = () => {
 
 const closeSidebar = () => {
   sidebar.classList.remove("sidebar-active");
-  container.classList.remove("no-scrolling");
   mobileOverlay.style.display = "none";
 };
