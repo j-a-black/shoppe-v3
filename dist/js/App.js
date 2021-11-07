656945a34d200e318ac9af6a5d608d4788609bd3
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2021-11-06 19:38:19 -0500

    blurred bkg when mobile menu is active; menu closes when link/overlay/arrow is clicked

--- dist/js/App.js
+++ dist/js/App.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// import { handleIconClick } from "./MobileMenu.js";
+import { handleIconClick } from "./MobileMenu.js";
 
 window.addEventListener("DOMContentLoaded", initApp);
 
@@ -8,8 +8,3 @@ function initApp() {
   const navIcon = document.querySelector(".mobile-menu-icon");
   navIcon.addEventListener("click", handleIconClick);
 }
-
-const handleIconClick = () => {
-  const sidebar = document.querySelector(".sidebar");
-  console.log(sidebar);
-};
--- dist/js/MobileMenu.js
+++ dist/js/MobileMenu.js
@@ -1,3 +1,24 @@
-// export const handleIconClick = () => {
-//   console.log("clicked on mobile icon");
-// };
+const sidebar = document.querySelector(".sidebar");
+const mobileOverlay = document.querySelector(".mobile-overlay");
+const sidebarIcon = document.querySelector(".sidebar__icon");
+const container = document.querySelector(".container");
+const mobileLinks = document.querySelectorAll(".sidebar__link");
+
+export const handleIconClick = () => {
+  sidebar.classList.add("sidebar-active");
+  mobileOverlay.style.display = "block";
+  sidebarIcon.addEventListener("click", closeSidebar);
+  mobileOverlay.addEventListener("click", closeSidebar);
+  container.classList.add("no-scrolling");
+
+  for (let i = 0; i < mobileLinks.length; i++) {
+    let link = mobileLinks[i];
+    link.addEventListener("click", closeSidebar);
+  }
+};
+
+const closeSidebar = () => {
+  sidebar.classList.remove("sidebar-active");
+  container.classList.remove("no-scrolling");
+  mobileOverlay.style.display = "none";
+};
